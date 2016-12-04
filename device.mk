LineageOS/android_device_huawei_angler

Fixed thermal HAL, added skin temperature, thresholds.
Bug: 27424857
Change-Id: Ia28f9de4305523bc6149f3f1d12fad400df15370

device.mk
@@ -250,7 +250,7 @@ PRODUCT_PACKAGES += \
 # Power HAL
 PRODUCT_PACKAGES += \
     power.angler \
-    hardware_properties.angler
+    thermal.angler
 
 PRODUCT_COPY_FILES += \
     device/huawei/angler/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \

thermal/Android.mk
@@ -19,8 +19,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_CFLAGS := -Wno-unused-parameter
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := hardware_properties.c
+LOCAL_SRC_FILES := thermal.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := hardware_properties.angler
+LOCAL_MODULE := thermal.angler
 include $(BUILD_SHARED_LIBRARY)

TWINNFAMOUS/android_device_htc_pme-1

device.mk
@@ -219,8 +219,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    hardware_properties.msm8996 \
-    power.msm8996
+    power.msm8996 \
+    thermal.msm8996
 
 # QMI
 PRODUCT_PACKAGES += \

thermal/Android.mk
@@ -19,8 +19,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_CFLAGS := -Wno-unused-parameter
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := hardware_properties.c
+LOCAL_SRC_FILES := thermal.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := hardware_properties.msm8996
+LOCAL_MODULE := thermal.msm8996
 include $(BUILD_SHARED_LIBRARY)